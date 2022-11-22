cbadb51d54280b99f75f6657011b5cfecf191b0d
Alberto Monroy <firewind3882@gmail.com>
2022-11-21 22:06:58 -0600

    added the updatePost function and validation for mongoose types and added a background for index.js on client side

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,27 +2,39 @@
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
-   try {
+  try {
     //Finding something in a model takes time so we add aysnc/await
     const postMessages = await PostMessage.find();
 
     //return the result
-    console.log (postMessages);
+    console.log(postMessages);
     res.status(200).json(postMessages);
-
-   } catch (error) {
-    res.status(404).json({message: error.message})
-   }
-}
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 export const createPost = async (req, res) => {
-    //adding different posts
-    const post = req.body;
-    const newPost = new PostMessage(post);
-    try {
-        await newPost.save();
-        res.status(201).json(newPost)
-    } catch (error) {
-        res.status(409).json({message: error.message})
-    }
-}
+  //adding different posts
+  const post = req.body;
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with this ID");
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
+  res.json(updatedPost);
+};
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,13 +1,13 @@
-import express from 'express';
+import express from "express";
 
 //import logic of routes
-import { getPosts, createPost} from '../controllers/posts.js'; //node needs the .js
+import { getPosts, createPost, updatePost } from "../controllers/posts.js"; //node needs the .js
 
 const router = express.Router();
 
 //Seperate accecible routes logic into controllers file.
-router.get('/', getPosts);
-router.get('/', createPost);
-
+router.get("/", getPosts);
+router.get("/", createPost);
+router.patch("/:id", updatePost);
 
 export default router;
