d4cbc2dcc24d7b384220e2e420070ff38ff1586f
Jorge Besnier <jorgebesnierb@outlook.com>
2022-11-27 11:34:46 -0600

    change some styles to make it a little bit better

--- client/src/components/PostDetails/CommentSection.jsx
+++ client/src/components/PostDetails/CommentSection.jsx
@@ -1,73 +1,51 @@
-//importing react, and some hooks
-import React, {useState, useRef} from 'react'
+import React, { useState, useRef } from 'react';
+import { Typography, TextField, Button } from '@material-ui/core/';
+import { useDispatch } from 'react-redux';
 
-//importing Material UI components
-import { Typography, textfi, TextField } from '@material-ui/core'
-
-// importing reducers
-import {useDispatch} from 'react-redux'
-
-//To use our styles
+import { commentPost } from '../../actions/posts';
 import useStyles from './styles';
-import { Button } from 'bootstrap';
 
-//funcitonal react component
-const CommentSection = ({post}) => {
-    console.log('comment Section', post);
-    //calling as a hook
-    const classes = useStyles();
+const CommentSection = ({ post }) => {
+  const user = JSON.parse(localStorage.getItem('profile'));
+  const [comment, setComment] = useState('');
+  const dispatch = useDispatch();
+  const [comments, setComments] = useState(post?.comments);
+  const classes = useStyles();
+  const commentsRef = useRef();
 
-    const [comments, setComments] = useState([])
+  const handleComment = async () => {
+    const newComments = await dispatch(commentPost(`${user?.result?.name}: ${comment}`, post._id));
 
-    //track the value of the comment
-    const [comment, setComment] = useState('')
+    setComment('');
+    setComments(newComments);
 
-    const handleClick = () => {
-        
-    }
+    commentsRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
 
-    // space for comment section
-        // 1. Define the layouts outer and inner div
-        // 2. Comments and a margin
-        // 3. Future of adding comments and looping
-
-    // space for adding a comment
-    //     1. define the layout
-    //     2. add text area for typing
-    return(
-        <div>
-            <div className={classes.commentsOuterContainer}>
-                <div className= {classes.commentsInnerContainer}>
-                    <Typography gutterBottom variant = "h6">Comments</Typography>
-                    {comments.map((c,i) => (
-                        <Typography key = {i} gutterBottom variant = "subtitle1">
-                            Comment{i}
-                        </Typography>
-                    ))}
-                </div>
-                <div style = {{width: 70%}}>
-                    <Typography gutterBottom variant = "h6">Write a comment</Typography>
-                    <TextField
-                        fullWidth
-                        rows={4}
-                        variant = "outlined"
-                        label = "Comment"
-                        multiline
-                        value = {comment}
-                        onChange = {(e) => setComment(e.target.value)}
-                    />
-                    <Button style = {{marginTop:'10px'}} fullWidth disabled = {!comment} variant = 'contained' onClick={handleClick}>
-                        post
-                    </Button>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className={classes.commentsOuterContainer}>
+        <div className={classes.commentsInnerContainer}>
+          <Typography gutterBottom variant="h6">Comments</Typography>
+          {comments?.map((c, i) => (
+            <Typography key={i} gutterBottom variant="subtitle1">
+              <strong>{c.split(': ')[0]}</strong>
+              {c.split(':')[1]}
+            </Typography>
+          ))}
+          <div ref={commentsRef} />
         </div>
-    )
+        <div style={{ width: '70%' }}>
+          <Typography gutterBottom variant="h6">Write a comment</Typography>
+          <TextField fullWidth rows={4} variant="outlined" label="Comment" multiline value={comment} onChange={(e) => setComment(e.target.value)} />
+          <br />
+          <Button style={{ marginTop: '10px' }} fullWidth disabled={!comment.length} color="primary" variant="contained" onClick={handleComment}>
+            Comment
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CommentSection;
-
-
-// Pending
-// 1. add comment section in post details
-// 2. add prop
